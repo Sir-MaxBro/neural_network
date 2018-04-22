a2ca5d33b316dcb71874434ecab53a38cf5b4107
Maksim Chendrov <maxbrofenx@gmail.com>
2018-04-22 15:26:01 +0300

    add neural library

---
+++ NeuralCompressed/NeuralCompressed.NeuralNetwork/Entities/NeuronType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralCompressed.NeuralNetwork.Entities
+{
+    public enum NeuronType
+    {
+        Hidden,
+        Output
+    }
+}
